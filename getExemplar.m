b728e329710d253b9548723162c63b92a9d9669a
Oliver Wei <gormory@Outlook.com>
2018-07-29 01:28:16 -0700

    Create getExemplar.m

---
+++ getExemplar.m
@@ -0,0 +1,17 @@
+function [q] = getExemplar(im,Ip,fillRegion,sourceRegion)
+% r and c are patch location
+% fillRegion contains to-fill point index
+% souceRegion is original part of img(logical values)
+[h,w,~] = size(im);
+examplar_location = exemplarhelper(h,w,size(Ip,1),size(Ip,2),Ip,fillRegion,sourceRegion);
+r = examplar_location(1) : examplar_location(2);
+c = examplar_location(3) : examplar_location(4);
+q = zeros(size(r,2),size(c,2));
+i = 1;
+for j = c
+    for k = r
+        q(i) = (j - 1) * h + k;
+        i = i + 1;
+    end
+end
+end
