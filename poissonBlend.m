bd9c9bfa4ffcf35aa4680a191a16e57d8c0df3a0
Oliver Wei <gormory@Outlook.com>
2018-07-31 19:18:46 -0700

    Update poissonBlend.m

--- poissonBlend.m
+++ poissonBlend.m
@@ -1,74 +1,66 @@
-function im_blend = poissonBlend(im_s, mask_s, im_background)
-    im_blend = zeros( size(im_background) );
-    poisson_mask = zeros( size(mask_s,1), size(mask_s,2) );
-    [my, mx] = find(mask_s);
-    len = length(mx);
-    for i = 1:len
-        poisson_mask( my(i), mx(i) ) = i;
+function [img] = poissonBlend(s,mask,t)
+[imh,imw,imb] = size(t);
+im2var = zeros(imh,imw);
+img = t .* (1 - mask);
+%building map;
+[yy,xx] = find(mask>0);
+counts = size(yy,1);
+e = 1;
+v = {};
+for i = 1:counts
+    im2var(yy(i),xx(i)) = e;
+    e = e + 1;
+end
+%rgb
+for c = 1:imb
+    A = sparse([], [], [], counts, counts );
+    b = zeros(counts,1);
+    e = 1;
+   for i = 1:counts
+       y = yy(i);
+       x = xx(i);
+       A(e,im2var(y,x)) = 4;
+       if(y >= imh)
+           y = imh - 1;
+       end
+       if(x >= imw)
+           x = imw - 1;
+       end
+       
+       if(mask(y - 1,x))
+           A(e,im2var(y - 1,x)) = -1;
+           b(e) = b(e) + s(y,x,c) - s(y - 1,x,c); 
+       else
+           b(e) = b(e) + t(y - 1,x,c); 
+       end
+       
+       if(mask(y + 1,x))
+           A(e,im2var(y + 1,x)) = -1;
+           b(e) = b(e) + s(y,x,c) - s(y + 1,x,c); 
+       else
+           b(e) = b(e) + t(y + 1,x,c); 
+       end
+       
+       if(mask(y,x - 1))
+           A(e,im2var(y,x - 1)) = -1;
+           b(e) = b(e) + s(y,x,c) - s(y,x - 1,c); 
+       else
+           b(e) = b(e) + t(y,x - 1,c); 
+       end
+       
+       if(mask(y,x + 1))
+           A(e,im2var(y,x + 1)) = -1;
+           b(e) = b(e) + s(y,x,c) - s(y,x + 1,c); 
+       else
+           b(e) = b(e) + t(y,x + 1,c); 
+       end
+       e = e+1;
+   end
+   v{c} = lscov(A, b); 
+end
+for i = 1:counts
+    for c = 1:imb
+        img(yy(i),xx(i),c) = v{c}(i);
     end
-    
-    for i = 1:3
-        background = im_background(:, :, i);
-        foreground = im_s(:, :, i);
-        e = 1;
-        A = sparse([],[],[],len*4+1, len); % each pixel in the target object needs 4 rows in matrix A to
-        b = sparse([],[],[],len*4+1, len); % define its left, right, up, down 
-        for j = 1:len
-            x = mx(j);
-            y = my(j);
-            %up
-            if (poisson_mask(y-1, x) == 0) 
-                A(e, poisson_mask(y, x)) = 1;
-                b(e) = foreground(y,x) - foreground(y-1,x) + background(y-1,x);
-            else
-                A(e, poisson_mask(y, x)) = 1;
-                A(e, poisson_mask(y-1, x)) = -1;
-                b(e) = foreground(y,x) - foreground(y-1,x);
-            end
-            e = e+1;
-            
-            %down
-            if (poisson_mask(y+1, x) == 0)
-                A(e, poisson_mask(y, x)) = 1;
-                b(e) = foreground(y,x) - foreground(y+1,x) + background(y+1,x);
-            else
-                A(e, poisson_mask(y, x)) = 1;
-                A(e, poisson_mask(y+1, x)) = -1;
-                b(e) = foreground(y,x) - foreground(y+1,x);
-            end
-            e = e+1;
-            
-            %left
-            if (poisson_mask(y, x-1) == 0)
-                A(e, poisson_mask(y, x)) = 1;
-                b(e) = foreground(y,x) - foreground(y,x-1) + background(y,x-1);
-            else
-                A(e, poisson_mask(y, x)) = 1;
-                A(e, poisson_mask(y, x-1)) = -1;
-                b(e) = foreground(y,x) - foreground(y,x-1);
-            end
-            e = e+1;
-            
-            %right
-            if (poisson_mask(y, x+1) == 0)
-                A(e, poisson_mask(y, x)) = 1;
-                b(e) = foreground(y,x) - foreground(y,x+1) + background(y,x+1);
-            else
-                A(e, poisson_mask(y, x)) = 1;
-                A(e, poisson_mask(y, x+1)) = -1;
-                b(e) = foreground(y,x) - foreground(y,x+1);
-            end
-            e = e+1;
-
-                        
-        end
-        A(e, poisson_mask(y, x)) = 1;
-        A = sparse(A);
-        b(e) = foreground(y, x);
-        v = A\b;
-        for k = 1:len
-            background( my(k), mx(k) )= v(k);
-        end
-        im_blend(:, :, i) = background;        
-    end
+end
 end
