d4917d713607f7acb603fc86af3ac4efb4ea1b2f
Oliver Wei <gormory@Outlook.com>
2018-08-02 15:58:49 -0700

    Update resizer.m

--- resizer.m
+++ resizer.m
@@ -2,8 +2,8 @@ function [resized,objmask] = resizer(im,mask,scaler)
 mask = double(mask);
 masked = double(im).*mask;
 [ux,uy,lx,ly] = getCorner(masked);
-resized = imresize(masked(uy:ly,ux:lx,:),scaler,'bilinear');
-objmask = imresize(mask(uy:ly,ux:lx,:),scaler,'bilinear');
+resized = imresize(masked(uy:ly,ux:lx,:),scaler,'nearest');
+objmask = imresize(mask(uy:ly,ux:lx,:),scaler,'nearest');
 
 %% for testing
 imshow(uint8(resized));
